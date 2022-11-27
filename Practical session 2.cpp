a4ebc4320c6ad06d9be61b21c550ff36bb87e2b6
RogierTimmer <timmer.rogier@gmail.com>
2022-11-27 17:00:46 +0100

    Begin on the question provided by teacher

---
+++ Practical session 2.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+#include "string"
+#include "cmath"
+#include "fstream"
+using namespace std;
+
+int main(){
+    bool x = true;
+    x = (int) x;
+
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>     // voeg de I/O bibliotheek toe (voor std::cin en std::cout)
 #include <string>       // voeg de string bibliotheek toe (voor std::string)
 #include <cmath>        // voeg iets laterige calculaties toe
+#include "fstream"
 
 using namespace std;    // maakt namespace std bruikbaar zonder de std::
 
 
-int main() {
+void myFunction() {
+    cout << "I just got executed! \n";
+}
+
+class MyClass {
+public:
+    int myNum;
+    string myString;
+};
+
+
+int main1() {
     cout << "Hello World! \n";
     cout << "I am learning C++\n";
     int mynum;
@@ -59,6 +71,50 @@ int main() {
         cout << i << "\n";
     }
 
+
+    struct {
+        int myNum;
+        string myString;
+    } myStructure;
+    myStructure.myNum = 10;
+    cout <<"myNum is " << myStructure.myNum << "\n";
+
+    struct {
+        string brand;
+        string model;
+        int year;
+    } myCar1, myCar2; // We can add variables by separating them with a comma here
+
+// Put data into the first structure
+    myCar1.brand = "BMW";
+    myCar1.model = "X5";
+    myCar1.year = 1999;
+
+// Put data into the second structure
+    myCar2.brand = "Ford";
+    myCar2.model = "Mustang";
+    myCar2.year = 1969;
+
+// Print the structure members
+    cout << myCar1.brand << " " << myCar1.model << " " << myCar1.year << "\n";
+    cout << myCar2.brand << " " << myCar2.model << " " << myCar2.year << "\n";
+
+    myFunction();
+
+    MyClass myObj; //creates an object of my class
+    myObj.myNum = 15;
+    myObj.myString = "Some text";
+    cout << myObj.myNum << "\n";
+    cout << myObj.myString << "\n";
+
+    // Create and open a text file
+    ofstream MyFile("filename.txt");
+
+    // Write to the file
+    MyFile << "Files can be tricky, but it is fun enough!";
+
+    // Close the file
+    MyFile.close();
 
 
     return 0;
